486c55c725e1ea6f79e502dc832015d3f1d7d5aa
sergTesst <sergiycheck@gmail.com>
2021-11-19 17:21:27 +0200

    changed font styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo, useRef } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import UserList from "../Users/UserList";
-import { chunk } from "lodash";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AlphabetElements } from "../FilterElements/FilterElements";
 
 function App() {
   return (
@@ -20,55 +20,5 @@ function App() {
     </div>
   );
 }
-
-export const AlphabetElements = () => {
-  const onLetterClick = (letterValue: string) => {
-    console.log("button click letter value", letterValue);
-  };
-  const groupingNum = 3;
-
-  const memoAlpabetArr = useMemo(function getAlphabetArr() {
-    let i: number;
-    const alpabetArr = [...Array(26)].map((_) => (++i).toString(36), (i = 9));
-
-    const chunks = chunk(alpabetArr, groupingNum);
-    const reducedChunks = chunks.reduce((prev, curr) => {
-      prev = prev.concat(curr.join(" "));
-      return prev;
-    }, []);
-
-    return reducedChunks;
-  }, []);
-
-  const renderedButtons = memoAlpabetArr.map((groupChars, i) => {
-    return (
-      <ElementWithLetter key={i} letter={groupChars} clickLetterCallBack={onLetterClick}>
-        <>{groupChars} </>
-      </ElementWithLetter>
-    );
-  });
-
-  return <div>{renderedButtons}</div>;
-};
-
-type elementWithLetterProps = {
-  letter: string;
-  clickLetterCallBack: (letterValue: string) => void;
-  children: JSX.Element;
-};
-export const ElementWithLetter = (props: elementWithLetterProps) => {
-  const { letter, clickLetterCallBack, children } = props;
-
-  const refLetter = useRef(letter);
-  const onButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const value = refLetter.current;
-    clickLetterCallBack(value);
-  };
-  return (
-    <button type="button" onClick={onButtonClick}>
-      {children}
-    </button>
-  );
-};
 
 export default App;
