8719f61f4fbe23ab7d392635ed94d9101c8c802d
sergTesst <sergiycheck@gmail.com>
2021-11-20 17:49:47 +0200

    change css to scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import VisibleUserList from "../Users/VisibleUserList";
 import { AlphabetElements } from "../FilterElements/FilterElements";
 import { ActiveUserList } from "../Users/ActiveUsersList";
